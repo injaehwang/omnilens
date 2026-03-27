5b14bab45413dfaa44677f4b406f87f42551b2f0
thinkij <thinkij@levelthree.kr>
2026-03-27 12:25:31 +0900

    Fix: install.js downloads v1.0.8 binary (was stuck on v1.0.2)

--- npm/omnilens/install.js
+++ npm/omnilens/install.js
@@ -6,7 +6,7 @@ const { existsSync, mkdirSync, chmodSync, unlinkSync, renameSync } = require("fs
 const { join } = require("path");
 const { execSync } = require("child_process");
 
-const VERSION = "v1.0.2";
+const VERSION = "v1.0.8";
 const REPO = "injaehwang/omnilens";
 
 const PLATFORMS = {
